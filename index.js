435fa7ccc2640354a70cab9dc87a3da1bf4c3006
Megan <dommelmeg@gmail.com>
2022-09-23 14:51:26 -0400

    commit tree 2fb39052461b8a9362a247f487d99bcf69fb7168

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === whisper(string)) {
+        return "I can't hear you!";
+    }
+
+    if (string === shout(string)) {
+        return "YES INDEED!";
+    }
+
+    if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
+
+const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase());
